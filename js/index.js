7960f773e1b6278aaeacfaff30f0664a1acdac2f
Gamaliel Sirengo <sirengogamaliel@gmail.com>
2022-08-03 23:39:08 +0300

    Understanding Uninitialized Variables

--- js/index.js
+++ js/index.js
@@ -2,3 +2,13 @@
 var myFirstName = "Gama";
 var myLastName = "Sirengo";
 console.log(myFirstName, myLastName)
+
+//Understanding Uninitialized Variables
+var a = 5;
+var b = 10;
+var c = 'I am a';
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
