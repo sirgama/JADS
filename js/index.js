74549c2b443453f9348d67f619e0c3e5c78a6881
Gamaliel Sirengo <sirengogamaliel@gmail.com>
2022-08-03 23:42:53 +0300

    Understanding Case Sensitivity in Variables

--- js/index.js
+++ js/index.js
@@ -7,8 +7,15 @@ console.log(myFirstName, myLastName)
 var a = 5;
 var b = 10;
 var c = 'I am a';
-// Only change code above this line
 
 a = a + 1;
 b = b + 5;
 c = c + " String!";
+
+//Understanding Case Sensitivity in Variables
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
