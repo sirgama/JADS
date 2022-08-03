d48b3e1986ff22a6b31e0012b80a83930b275b31
Gamaliel Sirengo <sirengogamaliel@gmail.com>
2022-08-03 23:57:56 +0300

    Declare a Read-Only Variable with the const Keyword

--- js/index.js
+++ js/index.js
@@ -23,3 +23,9 @@ titleCaseOver = 9000;
 //Explore Differences Between the var and let Keywords
 let catName = "Oliver";
 let catSound = "Meow!";
+
+//Declare a Read-Only Variable with the const Keyword
+const FCC = "freeCodeCamp"; 
+let fact = "is cool!";
+fact = "is awesome!";
+console.log(FCC, fact); 
