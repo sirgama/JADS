6a1798487b9373dc3c791d773736c2fc24634050
Gamaliel Sirengo <sirengogamaliel@gmail.com>
2022-08-04 17:21:42 +0300

    Modify Array Data With Indexes

--- js/index.js
+++ js/index.js
@@ -169,3 +169,9 @@ const arra = [11,12,23,345,56];
 let mstr1 = mstr[3]
 let arra1 = arra[3]
 console.log(mstr1, arra1)
+
+//Modify Array Data With Indexes
+
+const ourArray = [50, 40, 30];
+ourArray[0] = 15;
+console.log(ourArray)
